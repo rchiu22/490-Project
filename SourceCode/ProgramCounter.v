cef26d4db1beeb5e4767e356924f797f865104e0
rchiu22 <157850165+rchiu22@users.noreply.github.com>
2026-03-24 17:11:16 -0400

    Add files via upload

---
+++ SourceCode/ProgramCounter.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/23/2026 03:32:14 PM
+// Design Name: 
+// Module Name: ProgramCounter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ProgramCounter(
+    input wire [15:0] InstAdd,
+    output reg [15:0] Instruction
+    );
+    
+    
+endmodule
---
+++ SourceCode/SignExtend.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/13/2026 09:42:47 PM
+// Design Name: 
+// Module Name: SignExtend
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SignExtend(
+    input wire [7:0] Input,
+    output reg [15:0] se
+    );
+
+    always @(*)
+    assign se = {{12{Input[7]}}, Input};
+    
+ endmodule   
