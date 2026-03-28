4798c1991d2da3486626e158d833cd3130c9d291
rchiu22 <157850165+rchiu22@users.noreply.github.com>
2026-03-27 22:17:06 -0400

    IM tests concactenation of the two lines by PC

---
+++ SourceCode/IM_tb.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/27/2026 09:24:56 PM
+// Design Name: 
+// Module Name: IM_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module IM_tb;
+
+     reg [15:0] Read_Address;
+    wire [15:0] Instruction;
+    
+    InstructionMemory i1(Read_Address,Instruction);
+    
+    integer i;
+    initial
+    begin
+    //for (i = 0; i < 1; i = i + 1) begin
+    Read_Address = 0;   
+    #10;                // wait for memory to output
+    $display("Read_Address=%b Instruction=%b", Read_Address, Instruction);
+ 
+    
+    Read_Address = 2;   
+    #10;                // wait for memory to output
+    $display("Read_Address=%b Instruction=%b", Read_Address, Instruction);
+    $finish;
+    
+   
+        
+    end
+    //end
+    
+endmodule
