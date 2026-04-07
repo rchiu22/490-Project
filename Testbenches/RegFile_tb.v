2cb3f4239433b4ca6ffd79ed4444e5cb180352c9
Tomer Idi <124112775+Munchken105@users.noreply.github.com>
2026-04-06 23:09:12 -0400

    changed to latest version
    Refactor testbench initialization and register write values.

--- Testbenches/RegFile_tb.v
+++ Testbenches/RegFile_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/27/2026 10:31:45 PM
-// Design Name: 
-// Module Name: RegFile_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module RegFile_tb;
     reg clk;
@@ -29,23 +9,39 @@ module RegFile_tb;
     reg [15:0] Write_Data; // Data that is written back, 16 bits wide
     wire [15:0] Read_Data1; // Output1 of Read_Register1, sent through to ALU
     wire [15:0] Read_Data2; // Output2 of Read_Register2, can either be an address to write to in Data Memory, or second register value from Read_Register2
-  
-  Register_File rf(clk,RegWrite,Read_Register1,Read_Register2,Register_Write, Write_Data,Read_Data1,Read_Data2);
+    
+    Register_File rf(clk, RegWrite, Read_Register1, Read_Register2, Register_Write, Write_Data, Read_Data1, Read_Data2);
 
     always #5 clk = ~clk;
+    
     initial begin
-    clk = 0;
-    RegWrite = 0;
-    Register_Write = 3;
-    Write_Data = 16'd4;
-    Read_Register1 = 0;
-    Read_Register2 = 1;
-    end 
-    
-    
-   
-
-
+        clk = 0;
+        RegWrite = 0;
+        Register_Write = 4'b0;
+        Write_Data = 16'b0;
+        Read_Register1 = 4'b0;
+        Read_Register2 = 4'b0;
+        
+        // write 4 into register 3
+        #10;
+        RegWrite = 1;
+        Register_Write = 4'b0011;
+        Write_Data = 16'b0000000000000100;
+        #10;
+        RegWrite = 0;
+        Read_Register1 = 4'b0011;
+        Read_Register2 = 4'b0000;
+        // write 10 into register 5
+        #10;
+        RegWrite = 1;
+        Register_Write = 4'b0101;
+        Write_Data = 16'b0000000000001010;
+        #10;
+        RegWrite = 0;
+        Read_Register1 = 4'b0011;
+        Read_Register2 = 4'b0101;
+        #10;
+        $finish;
+    end
 
 endmodule
-
