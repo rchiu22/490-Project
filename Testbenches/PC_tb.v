e81491fb3065cc3b0f5d59abdca245b4c55fe660
Tomer Idi <124112775+Munchken105@users.noreply.github.com>
2026-04-06 23:10:28 -0400

    changed to latest version

--- Testbenches/PC_tb.v
+++ Testbenches/PC_tb.v
@@ -1,4 +1,28 @@
 `timescale 1ns / 1ps
+
+module ProgramCounter_tb;
+    reg clk;
+    reg reset;
+    reg [15:0] PCVal; // next PC val
+    wire [15:0] PCOutput; // current PC val
+
+    ProgramCounter p1(clk, reset, PCVal, PCOutput);
+    always #1 clk = ~clk;
+    initial begin
+        clk = 0;
+        reset = 1;
+        PCVal = 16'b0000000000000000;
+        #2;
+        reset = 0;
+        PCVal = 16'b0000000000000010;
+        #2;
+        PCVal = 16'b0000000000000100;
+        #2;
+        $finish;
+    end
+
+endmodule
+`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
